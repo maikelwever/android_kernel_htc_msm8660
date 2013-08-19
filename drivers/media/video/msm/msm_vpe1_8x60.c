4e737b7649637d33e4e981d72a54e85328463de6
Matt Filetto <matt.filetto@gmail.com>
2013-08-19 15:36:09 -0700

    Revert "Adapt more of the shooter camera hack to pyramid/holiday"
    This reverts commit 80a00d359b4d66c2ca53edca22ec74e8b5a7710b.

--- drivers/media/video/msm/msm_vpe1_8x60.c
+++ drivers/media/video/msm/msm_vpe1_8x60.c
@@ -33,8 +33,7 @@ static int vpe_update_scaler(struct video_crop_t *pcrop);
 static struct vpe_device_type  vpe_device_data;
 static struct vpe_device_type  *vpe_device;
 struct vpe_ctrl_type    *vpe_ctrl;
-extern char *vpe_general_cmd[];
-#if 0
+char *vpe_general_cmd[] = {
 	"VPE_DUMMY_0",  /* 0 */
 	"VPE_SET_CLK",
 	"VPE_RESET",
@@ -49,7 +48,6 @@ extern char *vpe_general_cmd[];
 	"VPE_AXI_OUT_CFG",
 	"VPE_CMD_DIS_OFFSET_CFG",
 };
-#endif
 static uint32_t orig_src_y, orig_src_cbcr;
 
 #define CHECKED_COPY_FROM_USER(in) {					\
